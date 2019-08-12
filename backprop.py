74a77df9b2e79cd133809d2377710386a9ad7aaf
Gadamsetty Sai Prakash <gadamsettysaiprakash@gmail.com>
2019-08-12 09:39:12 +0530

    Add files via upload

--- backprop.py
+++ backprop.py
@@ -42,7 +42,7 @@ def forward_prop(network,inrow):
 			wt_in = weight_input(neuron,inputs)
 			g = sigmoid(wt_in)
 			out_units.append(g)
-			print(out_units)
+			#print(out_units)
 		output.append(out_units)
 		inputs = out_units
 	return output
@@ -64,29 +64,47 @@ def error_cal(network,outputs,y):
 		h = y_xp[i]-y[i]
 		l_err.append(h)
 		
-	er1 = np.asarray(l_err)
+	er2 = np.asarray(l_err)
 	theta = network[1]
 	theta = np.asarray(theta)
-	e = np.dot(np.transpose(theta),np.transpose(er1))
-	print(e)
+	e = np.dot(np.transpose(theta),np.transpose(er2))
+	#print(e)
 	aj = outputs[0]
 	g_z_dash = g_dash_z(aj)
-	er2 = np.multiply(e,g_z_dash)
+	er1 = np.multiply(e,g_z_dash)
 	
-	return [er1,er2]	
+	return np.array([er1,er2])
+#calculation of delta
+def delta_cal(outputs,error,x):
+	outs = np.asarray(outputs)
+	err = np.asarray(error)
+	delta = list()
+	x.insert(0,1)
+	x = np.asarray(x)
+	h = np.asarray(outs[0]).reshape(6,1)
+	e = err[1].reshape(1,2)
+	d2 = h*e
+	d2 = d2.transpose()
+	e1 = err[0].reshape(1,6)
+	x= x.reshape(3,1)
+	d1 = x*e1
+	d1 = d1.transpose()
+	return [d1,d2]
 	
-	
-		
-
-
 
 netw = initial(2,5,2)
-print(netw)
+for layer in netw:
+	print(layer)
+print('\n')
 ins = [1,0.7]
 outs = forward_prop(netw,ins)
-print(outs)
+for k in outs:
+	print(k)
+print('\n')
 e = error_cal(netw,outs,[0,1])
 print(e)
+d = delta_cal(outs,e,[1,0.7])
+print(d)
 
 
 
