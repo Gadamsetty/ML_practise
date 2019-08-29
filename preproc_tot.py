5390452e44a84e00402a7ed359308f3bd4c3cd9a
Gadamsetty Sai Prakash <gadamsettysaiprakash@gmail.com>
2019-08-29 12:12:11 +0530

    Add files via upload

---
+++ preproc_tot.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os 
+import unicodedata as u
+import io
+import re
+
+p = '/home/siplab/Desktop/mannkibaat/telugu/text'
+p_w = '/home/siplab/Desktop/mannkibaat/telugu/text_w'
+for filename in os.listdir(p):
+	f = io.open(os.path.join(p,filename),'r+',encoding='utf-8')
+	fw = io.open(os.path.join(p_w,filename),'w',encoding='utf-8')
+	for i in f.readlines():
+		b = re.sub("\\.+\s*|\?|\!|\-|\'|\–|,|\/|:|\(|\)|\|\""," ",i)
+		b = b.replace(u'\u201c', ' ').replace(u'\u201d', ' ')
+		b = b.replace(u'\u2019', ' ').replace(u'\u2018', ' ')	
+		b = b.replace(u'\u2013', ' ')
+		b = b.replace(u"\u0964",' ')
+		b = b.replace(u"\u0022",' ')
+		fw.write(b)
